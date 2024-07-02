4090097744c2d76648e7e3a79a8159ce1d83958f
Paranoia8972 <contact@encryptopia.dev>
2024-07-02 21:06:18 +0200

    Sync Changelog from Pocketbase

--- src/components/page/changelog.tsx
+++ src/components/page/changelog.tsx
@@ -1,56 +1,79 @@
-import React from "react";
-import Link from "next/link";
+"use client";
+import React, { useState, useEffect } from "react";
 
-export default function ChangeLog() {
+interface ChangeLogItem {
+  id: string;
+  title: string;
+  content: string;
+  created: string;
+}
+
+function ChangeLog() {
+  const [changeLogItems, setChangeLogItems] = useState<ChangeLogItem[]>([]);
+
+  useEffect(() => {
+    const cacheKey = "changeLogCache";
+    const cacheExpiry = 24 * 60 * 60 * 1000;
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://pb.encryptopia.dev/api/collections/otp_changelog/records"
+      );
+      const data = await response.json();
+      setChangeLogItems(data.items);
+      const cacheData = {
+        items: data.items,
+        timestamp: new Date().getTime(),
+      };
+      localStorage.setItem(cacheKey, JSON.stringify(cacheData));
+    };
+
+    const cachedData = localStorage.getItem(cacheKey);
+    if (cachedData) {
+      const { items, timestamp } = JSON.parse(cachedData);
+      const isCacheValid = new Date().getTime() - timestamp < cacheExpiry;
+      if (isCacheValid) {
+        setChangeLogItems(items);
+        return;
+      }
+    }
+    fetchData().catch((error) =>
+      console.error("Error fetching changelog:", error)
+    );
+  }, []);
+
   return (
     <section className="py-10 px-4 bg-gray-950">
       <div className="container mx-auto px-4 py-10">
-        <h1 id="team" className="text-3xl font-bold mb-4">
+        <h1 id="changelog" className="text-3xl font-bold mb-4">
           CHANGELOG
         </h1>
-        {/*  */}
-        <div className="bg-[#1e1e1e] p-4 my-4 border-l-4 border-green-500">
-          <h3
-            className="text-lg font-bold text-green-500"
-            style={{
-              color: "#00de6d",
-              textShadow: "0 0 10px #00de6d",
-            }}>
-            Closed beta coming soon!
-          </h3>
-          <span className="text-xs text-gray-400">March 23, 2024</span>
-          <p className="text-sm mt-2">
-            We are proud to announce that the server is already in its final
-            phases. Unfortunately, some functions as well as those of your own
-            will require the time of the most patient of you, this means that we
-            are starting recruitment for the Closed Beta, if you are interested,
-            open a ticket on our{" "}
-            <Link
-              className="text-green-200 underline"
-              href={"https://discord.onthepixel.net"}>
-              Disord Server
-            </Link>
-            . Thank you for being with us.
-          </p>
-        </div>
-        {/*  */}
-        <div className="bg-[#1e1e1e] p-4 my-4 border-l-4 border-green-500">
-          <h3
-            className="text-lg font-bold text-green-500"
-            style={{
-              color: "#00de6d",
-              textShadow: "0 0 10px #00de6d",
-            }}>
-            New website!
-          </h3>
-          <span className="text-xs text-gray-400">March 17, 2024</span>
-          <p className="text-sm mt-2">
-            Welcome to the new OnThePixel website! Thank you for your patience.
-            I&#39;ll be adding more content to the website soon! You can expact
-            more information on our social media accounts in the coming weeks.
-          </p>
-        </div>
+        {changeLogItems.map((item) => (
+          <div
+            key={item.id}
+            className="bg-[#1e1e1e] p-4 my-4 border-l-4 border-green-500 rounded-lg"
+          >
+            <h3
+              className="text-lg font-bold text-green-500"
+              style={{ color: "#00de6d", textShadow: "0 0 10px #00de6d" }}
+            >
+              {item.title}
+            </h3>
+            <span className="text-xs text-gray-400">
+              {new Date(item.created).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </span>
+            <div
+              className="text-sm mt-2"
+              dangerouslySetInnerHTML={{ __html: item.content }}
+            />
+          </div>
+        ))}
       </div>
     </section>
   );
 }
+
+export default ChangeLog;
