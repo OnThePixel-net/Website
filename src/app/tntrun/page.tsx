45a39e199fa80d3b042e2d05dab1c595e2410faa
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2024-09-02 11:51:39 +0200

    Update tntrun page.tsx

--- src/app/tntrun/page.tsx
+++ src/app/tntrun/page.tsx
@@ -4,17 +4,19 @@ import TopPage from "@/components/page/top";
 export default function TNTRun() {
   return (
     <>
-      
-      <section className="bg-gray-900 pt-36 pb-20">
+      <TopPage />
+      <section className="bg-gray-950 pt-36 pb-20">
         <div className="container mx-auto px-4 text-center">
           <h1 className="text-4xl font-bold mb-6 text-white">TNT Run</h1>
-          <p className="text-gray-300 text-lg mb-8 max-w-3xl mx-auto leading-relaxed">
-            In TNT Run, you need to avoid falling TNT blocks and stay on the highest platform for as long as possible. Whether you're a beginner or a pro, there's always something for you!
+          <p className="text-gray-300 text-lg mb-8 max-w-3xl mx-auto">
+            In TNT Run, your goal is to avoid falling TNT and stay on the highest platform for as long as possible. Whether you're a beginner or a pro, there's always something for everyone!
           </p>
-          <p className="text-gray-300 text-lg mb-8 max-w-3xl mx-auto leading-relaxed">
-            Our TNT Run offers exciting PvP options and the ability to use Double Jumps to keep yourself from falling. Win the game and earn our server currency, "Pixels," to enhance your experience!
+          <p className="text-gray-300 text-lg mb-8 max-w-3xl mx-auto">
+            Our TNT Run features exciting PvP opportunities and the ability to use double jumps to avoid falling. Win the game and earn our server currency, "Pixels," to enhance your experience!
           </p>
-          
+          <a href="https://tntrun.de" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg shadow-lg">
+            Play Now
+          </a>
         </div>
       </section>
     </>
