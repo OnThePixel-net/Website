75cafdc8072c787763dad478b4c3245037ac404a
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2024-09-02 11:27:44 +0200

    Update page.tsx

--- src/app/tntrun/page.tsx
+++ src/app/tntrun/page.tsx
@@ -4,21 +4,18 @@ import TopPage from "@/components/page/top";
 export default function TNTRun() {
   return (
     <>
-      
+      <TopPage />
       <section className="bg-gray-950 pt-36 pb-20">
         <div className="container mx-auto px-4 text-center">
           <h1 className="text-4xl font-bold mb-6 text-white">TNT Run</h1>
-          <p className="text-gray-300 text-lg mb-8 max-w-2xl mx-auto">
-            Welcome to TNT Run! Your challenge: escape the falling TNT blocks and stay on the highest platform for as long as possible. Whether you're a seasoned pro or a newbie, there's always a thrill waiting for you.
+          <p className="text-gray-300 text-lg mb-8 max-w-3xl mx-auto">
+            In TNT Run geht es darum, dem unter dir fallenden TNT zu entkommen und so lange wie möglich auf der höchsten Plattform zu bleiben. Ob Anfänger oder Profi, bei uns ist für jeden etwas dabei!
           </p>
-          <p className="text-gray-300 text-lg mb-8 max-w-2xl mx-auto">
-            Dive into the excitement with PvP combat and Double Jumps to enhance your gameplay. Win and earn "Pixels," our server's currency, to unlock new features and rewards!
+          <p className="text-gray-300 text-lg mb-8 max-w-3xl mx-auto">
+            Unser TNT Run bietet dir spannende PvP-Möglichkeiten und die Chance, Double Jumps zu nutzen, um dich vor dem Sturz zu retten. Gewinne das Spiel und sammle die Server-Währung „Pixels“, um dich weiter zu verbessern!
           </p>
-          <a 
-            href="https://tntrun.de" 
-            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out"
-          >
-            Play Now
+          <a href="https://tntrun.de" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg shadow-lg">
+            Jetzt Spielen
           </a>
         </div>
       </section>
