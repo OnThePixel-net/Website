980b3c7c9bb31b1e1cfccf048b45b6ec7e56c2c5
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2024-09-02 12:40:12 +0200

    Update page.tsx

--- src/app/tntrun/page.tsx
+++ src/app/tntrun/page.tsx
@@ -9,10 +9,10 @@ export default function TNTRun() {
         <div className="container mx-auto px-4 text-center">
           <h1 className="text-4xl font-bold mb-6 text-white">TNT Run</h1>
           <p className="text-gray-300 text-lg mb-8 max-w-3xl mx-auto">
-            In TNT Run, your goal is to avoid falling TNT and stay on the highest platform for as long as possible. Whether you&apos;re a beginner or a pro, there&apos;s something for everyone!
+            In TNT Run, your goal is to avoid falling down and stay on the platforms for as long as possible. Whether you&apos;re a beginner or a pro, there&apos;s something for everyone!
           </p>
           <p className="text-gray-300 text-lg mb-8 max-w-3xl mx-auto">
-            Our TNT Run features exciting PvP opportunities and the chance to use Double Jumps to save yourself from falling. Win the game and earn our server currency, &quot;Pixels&quot;, to enhance your experience!
+            Our TNT Run features exciting PvP opportunities and the chance to use Double Jumps to save yourself from falling. Win the game and earn our server currency, Pixels, to enhance your experience!
           </p>
           ip: tntrun.de
         </div>
