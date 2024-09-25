80636f7543ed748af5a3c084f8b9fb0e2f9b9b4e
Paranoia8972 <contact@encryptopia.dev>
2024-09-25 21:34:38 +0200

    redirect links

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,25 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/whatsapp",
+        destination: "https://whatsapp.com/channel/0029VaA61GG84Om2YFUGV92N",
+        permanent: false,
+      },
+      {
+        source: "/twitter",
+        destination: "https://x.com/onthepixelnet",
+        permanent: false,
+      },
+      {
+        source: "/discord",
+        destination: "https://discord.onthepixel.net",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
