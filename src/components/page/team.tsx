7ba5744d5c61f5430044336cf4b125a155fd484b
Paranoia8972 <contact@encryptopia.dev>
2024-10-22 01:34:14 +0200

    handeling if no team members

--- src/components/page/team.tsx
+++ src/components/page/team.tsx
@@ -45,33 +45,37 @@ const Team = () => {
         <h1 id="team" className="text-3xl font-bold mb-4">
           TEAM
         </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {teamMembers.map((member) => (
-            <div
-              key={member.id}
-              className="bg-white/10 p-6 m-1 rounded-md flex items-center hover:scale-105 transition-transform duration-300"
-            >
-              <Image
-                alt={member.minecraft_name}
-                src={`https://minotar.net/helm/${member.minecraft_name}`}
-                width={40}
-                height={40}
-              />
-              <div className="ml-4">
-                <p className="font-bold">{member.discord_name}</p>
-                <p
-                  className="text-sm"
-                  style={{
-                    color: getRoleColor(member.role),
-                    textShadow: `0 0 10px ${getRoleColor(member.role)}`,
-                  }}
-                >
-                  {member.role.toUpperCase()}
-                </p>
+        {teamMembers.length === 0 ? (
+          <p className="text-center text-white">No team members found.</p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {teamMembers.map((member) => (
+              <div
+                key={member.id}
+                className="bg-white/10 p-6 m-1 rounded-md flex items-center hover:scale-105 transition-transform duration-300"
+              >
+                <Image
+                  alt={member.minecraft_name}
+                  src={`https://minotar.net/helm/${member.minecraft_name}`}
+                  width={40}
+                  height={40}
+                />
+                <div className="ml-4">
+                  <p className="font-bold">{member.discord_name}</p>
+                  <p
+                    className="text-sm"
+                    style={{
+                      color: getRoleColor(member.role),
+                      textShadow: `0 0 10px ${getRoleColor(member.role)}`,
+                    }}
+                  >
+                    {member.role.toUpperCase()}
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
