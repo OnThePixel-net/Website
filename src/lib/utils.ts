fc2622dba7ebcdfa855ad5173c0290f15a6afe7e
Paranoia8972 <contact@encryptopia.dev>
2024-11-19 19:21:13 +0100

    updated lib

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export function toInitials(name: string): string {
-  const words = name.trim().split(/\s+/);
-  if (words.length > 1) {
-    return words[0][0] + words[1][0];
-  } else {
-    return name.substring(0, 2);
-  }
 }
