e1e678461d51de15902df6b3f7f464911bf543de
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2025-02-06 12:37:56 +0100

    Create trailer.tsx

---
+++ src/components/page/trailer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Trailer() {
+  return (
+    <section className="py-10 px-4 bg-gray-950">
+      <div className="container mx-auto px-4 py-10">
+        <h1 id="changelog" className="text-3xl font-bold mb-4">
+          TRAILER ON RELEASE
+        </h1>
+      </div>
+    </section>
+  );
+}
