3377a37f43ca9790c1aa126bea2e2a22021a584a
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2025-03-09 05:23:54 +0100

    Update page.tsx

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,95 +1,15 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client";
 import React from "react";
 import TopPage from "@/components/page/top";
+import PlayerStatistics from "@/components/page/PlayerStatistics";
 
-export default function Leaderboards() {
+const StatisticsPage: React.FC = () => {
   return (
-    <>
+    <section className="bg-gray-950 min-h-screen">
       <TopPage />
-      <section className="bg-gray-950 pt-36">
-        <div className="container mx-auto px-4 py-10">
-          <h1 className="text-2xl font-bold mb-5">LEADERBOARDS</h1>
-          {/*  */}
-          <div className="flex py-4">
-            <div className="flex flex-col justify-center">
-              <Link href="/leaderboard/pixels">
-                <Image
-                  src={"/placeholder.png"}
-                  alt={"Pixels"}
-                  width={500}
-                  height={250}
-                  className="h-[200px] w-[400px] cursor-pointer rounded-xl border object-cover"
-                />
-              </Link>
-            </div>
-            <div className="ml-4">
-              <Link
-                href="/leaderboard/pixels"
-                className="text-xl font-bold mt-4 text-green-200 underline"
-              >
-                Pixels
-              </Link>
-              <p>
-                This is the Leaderboard for the Pixels, the currency of
-                OnThePixel.net
-              </p>
-            </div>
-          </div>
-          {/*  */}
-          <div className="flex py-4">
-            <div className="flex flex-col justify-center">
-              <Link href="/leaderboard/bedwars">
-                <Image
-                  src={"/bedwars.jpg"}
-                  alt={"BedWars"}
-                  width={500}
-                  height={250}
-                  className="h-[200px] w-[400px] cursor-pointer rounded-xl border object-cover"
-                />
-              </Link>
-            </div>
-            <div className="ml-4">
-              <Link
-                href="/leaderboard/bedwars"
-                className="text-xl font-bold mt-4 text-green-200 underline"
-              >
-                BedWars
-              </Link>
-              <p>
-                The Bedwars leaderboard ranks players by score, kills and
-                deaths.
-              </p>
-            </div>
-          </div>
-          {/*  */}
-          <div className="flex py-4">
-            <div className="flex flex-col justify-center">
-              <Link href="/leaderboard/parkour">
-                <Image
-                  src={"/parkour.png"}
-                  alt={"BedWars"}
-                  width={500}
-                  height={250}
-                  className="h-[200px] w-[400px] cursor-pointer rounded-xl border object-cover"
-                />
-              </Link>
-            </div>
-            <div className="ml-4">
-              <Link
-                href="/leaderboard/parkour"
-                className="text-xl font-bold mt-4 text-green-200 underline"
-              >
-                Parkour
-              </Link>
-              <p>
-                The Parkour Leaderboard ranks based on how far players have
-                progressed on the course.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
+      <PlayerStatistics />
+    </section>
   );
-}
+};
+
+export default StatisticsPage;
