f6aaed5081c935ec36cf5424a9b54d50e0ab72e2
Paranoia8972 <contact@encryptopia.dev>
2025-03-11 22:18:39 +0100

    feat(analytics): add Vercel Analytics component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/page/footer";
 import { SiteHeader } from "@/components/page/site-header";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <SiteHeader />
         <main>{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
