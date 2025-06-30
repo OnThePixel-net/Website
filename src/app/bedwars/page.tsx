81ad662dcdc469c757915fce338620ec63f91e53
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2025-06-30 15:42:42 +0200

    Update page.tsx

--- src/app/bedwars/page.tsx
+++ src/app/bedwars/page.tsx
@@ -3,6 +3,7 @@ import TopPage from "@/components/page/top";
 export default function Page() {
   return (
     <>
+      <TopPage />
       <section className="bg-gray-950 pt-36 min-h-screen text-white">
         <div className="container mx-auto px-4 py-10">
           <h1 className="text-2xl font-bold mb-5">BEDWARS</h1>
