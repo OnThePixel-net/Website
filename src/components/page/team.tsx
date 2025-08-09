922d434cac62d3c9b1eb2eeefdd9b848a8e1ec8a
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2025-08-09 14:53:30 +0200

    Update team.tsx

--- src/components/page/team.tsx
+++ src/components/page/team.tsx
@@ -91,7 +91,7 @@ export default function Team() {
               >
                 <Image
                   alt={member.minecraft_username}
-                  src={`https://vzge.me/face/512/{member.minecraft_username}.png`}
+                  src={`https://vzge.me/face/512/${member.minecraft_username}.png`}
                   width={40}
                   height={40}
                   className="rounded"
