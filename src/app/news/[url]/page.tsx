3ba7b465d117ab940f221fd752e700551467a790
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2025-08-09 04:15:38 +0200

    Update page.tsx

--- src/app/news/[url]/page.tsx
+++ src/app/news/[url]/page.tsx
@@ -82,7 +82,7 @@ export default function NewsPage() {
         <div className="container mx-auto px-4 py-10">
           <h1 className="text-2xl font-bold mb-5">{newsItem.title}</h1>
           <div className="mb-8 text-gray-300 text-lg leading-relaxed">
-            {newsItem.content}
+            {newsItem.text}
           </div>
         </div>
       </section>
