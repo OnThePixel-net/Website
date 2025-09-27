d80980d82004ab294ce7750c573cfdac62762216
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2025-09-28 00:01:31 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
