804148273adda5dd73f8c75a4c1e01f974696de0
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2026-03-19 11:37:31 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "OnThePixel.net",
   description:
-    "Explore the exciting world of OnThePixel.net! Engage in thrilling minigames like Bridging, TNTRun, KitPvP, BedWars, and BuildFFA. Dive into the pixelated fun today!",
+    "Explore the exciting world of OnThePixel.net! Engage in thrilling minigames like Duels and BuildFFA. Dive into the pixelated fun today!",
 };
 
 export default function RootLayout({
@@ -21,6 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://cms.onthepixel.net" />
+        <link rel="dns-prefetch" href="https://cms.onthepixel.net" />
+      </head>
       <body className={`${inter.className} scroll-smooth bg-gray-950`}>
         <SessionProvider>
           <AnalyticsProvider>
