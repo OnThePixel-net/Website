c669fdb015e26b54274e4536cfd8c5ae88802e78
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2026-03-30 14:56:46 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "@/components/page/header";
 import Trailer from "@/components/page/trailer";
 import Team from "@/components/page/team";
-import ChangeLog from "@/components/page/changelog";
+import News from "@/components/page/news";
 
 export default function Home() {
   return (
